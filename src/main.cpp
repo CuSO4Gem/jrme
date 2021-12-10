033b7ec71f65f1498176ae186504452c11686bf5
CuSO4Gem <328405829@qq.com>
2021-12-10 22:11:23 +0800

    代码可以运行

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <string>
 
@@ -9,18 +10,18 @@
 
 using namespace std;
 
-#define PLAG_NAME "zh_timparser"
+#define PLAG_NAME "timparser_zh"
 #define PLAG_SUBFIX "so"
 
 #include "timeparser_plugin.h"
 int main(int argc, char* argv[]) {
     void *tpHandle;
-    std::string pluginName = PLAG_NAME + "." + PLAG_SUBFIX;
+    std::string pluginName = string(PLAG_NAME) + "." + PLAG_SUBFIX;
     
     tpHandle = dlopen(pluginName.c_str(), RTLD_LAZY);
     if (!tpHandle)
     {
-		fprintf(stderr, "fail to load plugin: %s\n", plugin_name.c_str());
+		fprintf(stderr, "fail to load plugin: %s for %s\n", pluginName.c_str(), dlerror());
         return -1;
     }
     pParseTime parse = (pParseTime)dlsym(tpHandle, "parseTime");
@@ -28,8 +29,9 @@ int main(int argc, char* argv[]) {
     uint32_t flag = 0;
     std::string time_str = "today";
     struct tm timeOut;
-    memset(timeOut, 0, sizeof(timeOut));
+    memset(&timeOut, 0, sizeof(timeOut));
     ret = parse(time_str.c_str(), time_str.length(), &timeOut, &flag);
+    printf("timeOut.sec = %d\n", timeOut.tm_sec);
     dlclose(tpHandle);
     return 0;
 }
