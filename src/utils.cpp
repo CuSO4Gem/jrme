ff119fd868ba4395abd99d2ce86aeecab2e6539a
CuSO4Gem <328405829@qq.com>
2022-01-24 22:58:23 +0800

    utils configGetline

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,7 @@
-#include "utils.h"
+#include <iostream>
+#include <sstream>
 
+#include "utils.h"
 
 void removeMultipleSpaces(string &str){
     uint32_t i=0;
@@ -19,5 +21,77 @@ void removeMultipleSpaces(string &str){
         {
             i++;
         }
+    }
+}
+
+void tabToSpace(string &str)
+{
+    size_t pos;
+    while (pos<str.length())
+    {
+        if (str[pos] == '\t')
+        pos++;
+    }
+}
+
+bool configGetline(istringstream &configStream, string &key, string &value)
+{
+    string lineBuffer;
+    key.clear();
+    value.clear();
+    if(!getline(configStream, lineBuffer))
+        return false;
+    
+    if (lineBuffer.length() == 0)
+        return true;
+    
+    tabToSpace(lineBuffer);
+    size_t pos = 0;
+    while (pos<lineBuffer.length())
+    {
+        if (lineBuffer[pos]=='=')
+        {
+            key = lineBuffer.substr(0, pos);
+            break;
+        }
+        pos++;
+    }
+
+    lineBuffer.erase(0, min(pos+1, lineBuffer.length()));
+    
+    /*remove space front and back*/
+    while (key.length()>0 && key[0]==' ')
+    {
+        key.erase(0);
+    }
+    while (key.length()>0 && key[key.length()-1]==' ')
+    {
+        key.erase(key.length()-1);
+    }
+
+    if (lineBuffer.length()==0)
+        return true;
+    
+    value = lineBuffer;
+    /*remove space front and back*/
+    while (value.length()>0 && value[0]==' ')
+    {
+        value.erase(0);
+    }
+    while (value.length()>0 && value[value.length()-1]==' ')
+    {
+        value.erase(value.length()-1);
+    }
+    return true;
+}
+
+Date getJournalFormConfig(string &config)
+{
+    string lineBuffer;
+    istringstream configStream = istringstream(config);
+    
+    while(getline(configStream, lineBuffer))
+    {
+
     }
 }
--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,19 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "date.h"
+#include "Journal.h"
+
 #include <string>
 
 using namespace::std;
+using namespace::ec;
 
+/**
+ * @brief remove multiple space from string
+ */
 void removeMultipleSpaces(string &str);
+
+Date getJournalFormConfig(string &config);
 
 #endif
