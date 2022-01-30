211fcb8846c17a5c0c3b78cf1a3632f7eae0b4b7
zornlink.link <328405829@qq.com>
2022-01-30 15:18:16 +0800

    config node 插件可以编译

--- plugin/include/plugin_common.h
+++ plugin/include/plugin_common.h
@@ -3,15 +3,15 @@
 
 #ifdef WIN32
     #ifdef DLL_EXPORTS
-        #define NOIT_IT_API __declspec(dllexport)
+        #define JRME_PLUGIN_API __declspec(dllexport)
     #else
-        #define NOIT_IT_API
+        #define JRME_PLUGIN_API
     #endif // _MSC_VER
 #else
     #ifdef DLL_EXPORTS
-        #define NOIT_IT_API __attribute__((visibility("default")))
+        #define JRME_PLUGIN_API __attribute__((visibility("default")))
     #else
-        #define NOIT_IT_API
+        #define JRME_PLUGIN_API
     #endif
 #endif
 
--- plugin/timeparser/timeparser_en/timeparser_en.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIMEPARSER_EN_H
-#define TIMEPARSER_EN_H
-#include "timeparser_plugin.h"
-
-#include <string.h>
-
-#endif
