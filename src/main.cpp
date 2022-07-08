95518ad53fdc3507d1e29c81c529f668b708e9cc
CuSO4Gem <328405829@qq.com>
2022-07-08 23:35:15 +0800

    添加main.cpp的注释

--- src/main.cpp
+++ src/main.cpp
@@ -18,22 +18,23 @@ limitations under the License.
 
 using namespace std;
 
-#include "cmdline.h"
-#include "debug_print.h"
 #include "EnTimeParser.h"
 #include "JournalFilter.h"
 #include "JrmeConfig.h"
 #include "SearchMod.h"
 #include "Utils.h"
 #include "WriteMode.h"
+#include "cmdline.h"
+#include "debug_print.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     if (!JrmeConfig::installIfNeed())
     {
         JLOGE("[E] install jrme fail!!\n");
         return -1;
     }
-    
+
     cmdline::parser cmd;
     cmd.add<string>("title", 'T', "The title of journal", false, "");
     cmd.add<string>("at", 'A', "the time of journal", false, "");
@@ -54,61 +55,70 @@ int main(int argc, char* argv[]) {
     cmd.add("edit_book_list", 'B', "edit journal book path list");
 
     cmd.add("edit", 'E', "edit journal");
-    cmd.add("delete", 'D', "delate journal. If input -D true, jrme will delete journal without asking.");
-    cmd.add("force_delete", '\0', "force delate journal. If input -D true, jrme will delete journal without asking.");
+    cmd.add("delete", 'D',
+            "delate journal. If input -D true, jrme will delete journal "
+            "without asking.");
+    cmd.add("force_delete", '\0',
+            "force delate journal. If input -D true, jrme will delete "
+            "journal without asking.");
 
     cmd.add("version", '\0');
 
     cmd.parse_check(argc, argv);
 
     /* print version */
+    /* 打印版本号 */
     if (cmd.exist("version"))
     {
-#ifndef GIT_TAG_VERSION
         printf("jrme version 0.9.2.\ndebug level %d\n", DBG_PRINT_ENABLED);
-#else
-        printf("jrme version (get from git) %s \ndebug level %d\n", GIT_TAG_VERSION, DBG_PRINT_ENABLED);
-#endif
         return 0;
     }
-    /*Select a journal book as default*/
+    /* Select a journal book from journal books list config file as default */
+    /* 选择日记本列表配置文件中的以供日记本，作为默认日记本 */
     if (cmd.exist("default_book"))
     {
+        /* Get all journal books path and print them one by one whith order number ahead */
+        /* 获取所有的日记本路径并依次打印 ,前面附带顺序  */
         list<string> allBooksPath = JrmeConfig::readJournalBooksCfg();
-        printf("input a number to select the default journal book\n");
-        size_t i=0;
-        for (auto &it:allBooksPath)
+        printf("input a number to select the default journal books\n");
+        size_t i = 0;
+        for (auto &it : allBooksPath)
         {
-            printf("[%ld] %s\n", i,it.c_str());
+            printf("[%ld] %s\n", i, it.c_str());
             i++;
         }
         printf(":");
+
+        /* Get the input number of journal book by user */
+        /* 让用户输入一个数字作用户所选择的默认日记本  */
         string answer;
         getline(cin, answer);
-        if (answer.length()==0)
+        if (answer.length() == 0)
             return 0;
         int number = atoi(answer.c_str());
-        if (number<0 || number>=allBooksPath.size())
+        if (number < 0 || number >= allBooksPath.size())
         {
             printf("Invalid input\n");
             return -1;
         }
         list<string>::iterator strIt;
         strIt = allBooksPath.begin();
-        for (i=0; i<number; i++)
+        for (i = 0; i < number; i++)
         {
             strIt++;
         }
         JrmeConfig::setDeafultJournalBookPath(*strIt);
         return 0;
     }
-    
-    /*Add book path to journal book path list*/
+
+    /* Add book path to journal books path list config file*/
+    /* 添加一个新的日记本路径到日记本列表配置文件 */
     if (cmd.exist("add_book"))
     {
         string path = cmd.get<string>("add_book");
+
         path = validPath(path);
-        if (path.size()==0)
+        if (path.size() == 0)
         {
             printf("Invalid input\n");
             return -1;
@@ -119,73 +129,88 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    /*Edit journal book path list config file*/
+    /* Edit journal books path list config file */
+    /* 修改日记本文件列表  */
     if (cmd.exist("edit_book_list"))
     {
-        string editorName = JrmeConfig::getEditorName();
+        string editorName  = JrmeConfig::getEditorName();
         string bookListCfg = JrmeConfig::getJournalBooksCfgPath();
-        int sys_ret;
+        int    sys_ret;
+
+        /* Call default editor to edit journal books list config file */
+        /* 系统调用编辑器，以此编辑日记列表文件 */
         sys_ret = system((editorName + " " + bookListCfg).c_str());
         return 0;
     }
 
-    /** Select a journal book */
+    /** Select a journal book to operate. --book and -- path opthins can select a journl book.
+     * If none any opthins selected, jrme will select the default journal book.
+     */
+    /* 选择一个日记本开始操作，--book和--path都可以指定日记本，如果这两个参数都没有，则会选择默认的日记本 */
     string journalBookPath;
+    /* Print all journal books in journal books config file. Select one to operate*/
+    /* 打印日记本列表配置文件中的日记本以供用户选择其一，作为所需操作的日记本 */
     if (cmd.exist("book"))
     {
-        /** Let user to select a journal book from joruanl book cfg.
-         */
+        /* Get all journal books path and print them one by one whith order number ahead */
+        /* 获取所有的日记本路径并依次打印 ,前面附带顺序  */
         list<string> allBooksPath = JrmeConfig::readJournalBooksCfg();
-        if (allBooksPath.size()==0)
+        if (allBooksPath.size() == 0)
         {
             journalBookPath = JrmeConfig::getDefaultJournalBookPath();
-            printf("There are no any journal book. Please add journal book by option -a.\n");
+            printf("There are no any journal book. Please add "
+                   "journal book by option -a.\n");
             return -1;
         }
         printf("input a number to select a journal book\n");
-        size_t i=0;
-        for (auto &it:allBooksPath)
+
+        /* Get the input number of journal book by user as selected journal book */
+        /* 让用户输入一个数字以 选择的默认日记本  */
+        size_t i = 0;
+        for (auto &it : allBooksPath)
         {
-            printf("[%ld] %s\n", i,it.c_str());
+            printf("[%ld] %s\n", i, it.c_str());
             i++;
         }
         printf(":");
         string answer;
         getline(cin, answer);
-        if (answer.length()==0)
+        if (answer.length() == 0)
         {
             printf("Invalid input\n");
             return -1;
         }
-        if (answer.length()==0 || !isNumChar(answer[0]))
+        if (answer.length() == 0 || !isNumChar(answer[0]))
         {
             printf("Invalid input\n");
             return -1;
         }
         int number = atoi(answer.c_str());
-        if (number<0 || number>=allBooksPath.size())
+        if (number < 0 || number >= allBooksPath.size())
         {
             printf("Invalid input\n");
             return -1;
         }
         list<string>::iterator strIt;
         strIt = allBooksPath.begin();
-        for (i=0; i<number; i++)
+        for (i = 0; i < number; i++)
         {
             strIt++;
         }
         journalBookPath = *strIt;
     }
+    /* Select the book path which given by user, but check the path is valid */
+    /* 选择用户给定的日记本路径，但是要确保路径合理  */
     else if (cmd.exist("path"))
     {
         journalBookPath = cmd.get<string>("path");
-        if (journalBookPath.length()==0)
+        if (journalBookPath.length() == 0)
         {
             printf("input a null path\n");
             return -1;
         }
-        journalBookPath  = validPath(journalBookPath);
-        if (journalBookPath.length()==0)
+        journalBookPath = validPath(journalBookPath);
+        if (journalBookPath.length() == 0)
         {
             printf("option -p or --path get invalid input\n");
             return -1;
@@ -193,24 +218,28 @@ int main(int argc, char* argv[]) {
     }
     else
         journalBookPath = JrmeConfig::getDefaultJournalBookPath();
-    
-    /*write journal*/
+
+    /* If title, time or content have been given, that is mean that user wanna to wirte a journal */
+    /* 如果给定了标题、时间或者内容，则认为用户是要写日记 */
     int ret;
     if (cmd.exist("title") || cmd.exist("at") || cmd.exist("content"))
     {
-        ret = journlWriteMode(journalBookPath, cmd.get<string>("at"), cmd.get<string>("title"), cmd.get<string>("content"));
+        ret = journlWriteMode(journalBookPath, cmd.get<string>("at"), cmd.get<string>("title"),
+                              cmd.get<string>("content"));
         return 0;
     }
 
-    
-    if (cmd.exist("all_tags") || cmd.exist("tags")||
-        cmd.exist("level")    || cmd.exist("on")  ||
-        cmd.exist("from")     || cmd.exist("to")  ||
-        cmd.exist("number"))
+    /* User wanner to search journal */
+    /* 用户希望搜索日记 */
+    if (cmd.exist("all_tags") || cmd.exist("tags") || cmd.exist("level") || cmd.exist("on") || cmd.exist("from") ||
+        cmd.exist("to") || cmd.exist("number"))
+    {
         ret = journalSearchMod(cmd, journalBookPath);
-    else
-        ret = journlWriteMode(journalBookPath, string(""), string(""), string(""));
-    
+        return 0;
+    }
+
+    /* User wanner to Write journal */
+    ret = journlWriteMode(journalBookPath, string(""), string(""), string(""));
 
     return ret;
 }
